9553200a8a565070ae159d3365fac80bfd8023da
Amila Sampath <pgsamila@yahoo.com>
2023-06-13 17:36:01 +1000

    Categorized components

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,11 +1,11 @@
 import { View, Text, SafeAreaView, ScrollView } from 'react-native'
 import React from 'react'
-import HeaderTabs from '../components/HeaderTabs'
-import SearchBar from '../components/SearchBar'
-import Categories from '../components/Categories'
-import RestaurantItems, { localRestaurants } from '../components/RestaurantItems'
 import { Divider } from 'react-native-elements'
-import BottomTabs from '../components/BottomTabs'
+import BottomTabs from '../components/Home/BottomTabs'
+import RestaurantItems, { localRestaurants } from '../components/Home/RestaurantItems'
+import HeaderTabs from '../components/Home/HeaderTabs'
+import SearchBar from '../components/Home/SearchBar'
+import Categories from '../components/Home/Categories'
 
 const HomeScreen = () => {
 
